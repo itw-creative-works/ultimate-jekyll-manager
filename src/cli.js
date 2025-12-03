aff670f020e620c2241785915d380990df12e6ca
Ian <ian.wiedenman@gmail.com>
2025-12-02 21:11:23 -0800

    Update

--- src/cli.js
+++ src/cli.js
@@ -8,7 +8,6 @@ const ALIASES = {
   clean: ['-c', '--clean'],
   'cloudflare-purge': ['-cf', 'purge', '--cloudflare-purge'],
   install: ['-i', 'i', '--install'],
-  repo: ['-r', '--repo'],
   setup: ['-s', '--setup'],
   version: ['-v', '--version'],
   // Tasks
